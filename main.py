fbd670e5d9e532e7ab6e7131996d67ac6e6d57fe
Yuuna Yanagida <66319933+y-yana@users.noreply.github.com>
2021-05-23 18:19:22 +0900

    Initial value test success

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from typing import Optional
 from fastapi import FastAPI
 from pydantic import BaseModel
 from scraping import scraping
@@ -8,13 +9,20 @@ app = FastAPI()
 
 # リクエストbodyを定義
 class User(BaseModel):
+    url: Optional[str] = None
     user_id: str
     user_password: str
+    show_chrome:  Optional[bool] = None
 
 
 # シンプルなJSON Bodyの受け取り
 @app.post("/user/")
 def create_user(user: User):
-    ans = scraping.rename(user.user_id)
+    #ans = scraping.rename(user.user_id)
+    if user.url == None:
+        user.url = 'https://hiro-unipa.itp.kindai.ac.jp/up/faces/up/po/Poa00601A.jsp'
+    if user.show_chrome == None:
+        user.show_chrome = False
+    ans = scraping.main(user.url, user.user_id, user.user_password, user.show_chrome)
     return ans
     #return {"res": "ok", "ID": user.user_id, "password": user.user_password}
--- scraping/scraping.py
+++ scraping/scraping.py
@@ -1,3 +1,6 @@
-def rename(name):
-  rename = name + name
-  return rename
+def main(url, name, ppass, chrome):
+  reurl = url
+  rename = name
+  repass = ppass
+  rechrome = chrome
+  return reurl, rename, repass, rechrome
