90d993931d291f09ec22e8edc596a5a64d3aca2c
Yuuna Yanagida <66319933+y-yana@users.noreply.github.com>
2021-05-23 20:24:05 +0900

    import scraping success

--- main.py
+++ main.py
@@ -18,11 +18,9 @@ class User(BaseModel):
 # シンプルなJSON Bodyの受け取り
 @app.post("/user/")
 def create_user(user: User):
-    #ans = scraping.rename(user.user_id)
     if user.url == None:
         user.url = 'https://hiro-unipa.itp.kindai.ac.jp/up/faces/up/po/Poa00601A.jsp'
     if user.show_chrome == None:
         user.show_chrome = False
     ans = scraping.main(user.url, user.user_id, user.user_password, user.show_chrome)
     return ans
-    #return {"res": "ok", "ID": user.user_id, "password": user.user_password}
